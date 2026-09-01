1b245b859c6695b71331c598c2b0d8e1c797f079
Francesco <francescoq8@gmail.com>
2026-09-01 15:52:46 +0200

    Dado più lento e leggibile

--- js/dado-ui.js
+++ js/dado-ui.js
@@ -26,7 +26,7 @@ export function animaDado(valoreFinale) {
     overlay.classList.remove('nascosta');
 
     let tiri = 0;
-    const TIRI_TOTALI = 10;
+    const TIRI_TOTALI = 12;
 
     const scatta = () => {
       faccia.classList.remove('dado-scatto');
@@ -47,8 +47,8 @@ export function animaDado(valoreFinale) {
         setTimeout(() => {
           overlay.classList.add('nascosta');
           risolvi();
-        }, 700);
+        }, 900); // resta un attimo sul numero vero prima di sparire
       }
-    }, 90);
+    }, 140);
   });
 }
